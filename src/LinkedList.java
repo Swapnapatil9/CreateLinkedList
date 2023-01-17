02252e540fea0314f57fbcca167b26608c4a0bb0
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-01-17 22:40:37 +0530

    Added: Create a linkedlist by appeding 30 and 70 to 56.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,22 +1,35 @@
 public class LinkedList<E> {
     Node<E> head;
     Node<E> tail;
-    public void push(E key){
-        Node<E> newNode= new Node<>(key);
-        if(head == null){
-            head=newNode;
-            tail=newNode;
+
+    public void push(E key) {
+        Node<E> newNode = new Node<>(key);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
         } else {
-            tail.next= newNode;
-            tail= newNode;
-        }
-    }
-    public void display(){
-        Node<E> temp=head;
-        while(temp!=null){
-            System.out.print(temp.key +" -> ");
-            temp=temp.next;
+            newNode.next = head;
+            head = newNode;
         }
     }
 
+    public void display() {
+        Node<E> temp = head;
+        while (temp != null) {
+            System.out.print(temp.key + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void append(E data) {
+        Node<E> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
 }
