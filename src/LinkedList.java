9057ba18e744a9cfb11a1b429a5fc899e2820e2d
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-01-18 19:10:55 +0530

    Added: Ability to delete the last element in the LinkedList of sequence 56->30->70

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,24 +2,6 @@ public class LinkedList<E> {
     Node<E> head;
     Node<E> tail;
 
-    public void push(E key) {
-        Node<E> newNode = new Node<>(key);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
-        }
-    }
-    public void display(){
-        Node<E> temp=head;
-        while(temp!=null){
-            System.out.print(temp.key +" -> ");
-            temp=temp.next;
-        }
-    }
-
     public void display() {
         Node<E> temp = head;
         while (temp != null) {
@@ -38,5 +20,17 @@ public class LinkedList<E> {
             tail.next = newNode;
             tail = newNode;
         }
+    }
+    public E popLast() {
+        if(head == null)
+            return null;
+        E popElement = tail.key;
+        Node<E> temp = head;
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+        return popElement;
     }
 }
--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -1,15 +1,22 @@
 public class LinkedListMain {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList1=new LinkedList<>();
-        linkedList1.push(70);
-        linkedList1.push(30);
-        linkedList1.push(56);
-        linkedList1.display();
 
         LinkedList<Integer> linkedList2=new LinkedList<>();
         linkedList2.append(56);
         linkedList2.append(30);
         linkedList2.append(70);
         linkedList2.display();
+
+        System.out.println();
+        System.out.println("After pop Last...");
+
+        Integer popLastElement = linkedList2.popLast();
+        if(popLastElement == null)
+            System.out.println("Linked List Empty");
+        else
+            System.out.println("Pop Last Element " + popLastElement);
+
+        linkedList2.display();
+
     }
 }
